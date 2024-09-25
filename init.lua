4cb14d761400e8477506e37f5ca2b085c5fa0c91
jeremyp <jpekmez@gmail.com>
2024-09-25 23:11:43 +0000

    fix: python env

--- init.lua
+++ init.lua
@@ -722,6 +722,36 @@ require('lazy').setup({
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+      -- LSP utility functions
+      local configs = require 'lspconfig/configs'
+      local util = require 'lspconfig/util'
+      local path = util.path
+
+      local function get_python_path(workspace)
+        -- Use activated virtualenv.
+        if vim.env.VIRTUAL_ENV then
+          return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+        end
+
+        -- Find and use virtualenv in workspace directory.
+        for _, pattern in ipairs { '*', '.*' } do
+          local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+          if match ~= '' then
+            return path.join(path.dirname(match), 'bin', 'python')
+          end
+        end
+
+        -- Find and use virtualenv via poetry in workspace directory.
+        local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+        if match ~= '' then
+          local venv = vim.fn.trim(vim.fn.system('poetry --directory ' .. workspace .. ' env info -p'))
+          return path.join(venv, 'bin', 'python')
+        end
+
+        -- Fallback to system Python.
+        return exepath 'python3' or exepath 'python' or 'python'
+      end
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -734,7 +764,11 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        pyright = {
+          before_init = function(_, config)
+            config.settings.python.pythonPath = get_python_path(config.root_dir)
+          end,
+        },
         rust_analyzer = {},
         bufls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
