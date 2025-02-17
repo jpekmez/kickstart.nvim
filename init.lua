a5fc9bcda08e669d9bfeb78e6ed77368eb6e5f57
jpekmez <jpekmez@cohere.com>
2025-02-17 22:02:24 +0100

    add ruff

--- init.lua
+++ init.lua
@@ -871,6 +871,7 @@ require('lazy').setup({
             config.settings.python.pythonPath = get_python_path(config.root_dir)
           end,
         },
+        ruff = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
