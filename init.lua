4d853b9f2306c5610aa15b25c06170f9ce147919
jpekmez <jpekmez@cohere.com>
2025-02-18 10:35:15 +0100

    feat: avante openai

--- init.lua
+++ init.lua
@@ -261,9 +261,17 @@ require('lazy').setup({
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
-      provider = 'cohere',
+      provider = 'openai',
       cohere = {
         model = 'command-r7b-12-2024',
+      },
+      openai = {
+        endpoint = 'https://api.openai.com/v1',
+        model = 'gpt-4o',
+        timeout = 30000,
+        temperature = 0,
+        max_tokens = 4096,
+        -- reasoning_effort = 'high',
       },
     },
   },
