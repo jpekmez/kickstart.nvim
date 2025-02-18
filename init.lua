6389853671454be5f5f3c5a69504c94a323cd06c
jpekmez <jpekmez@cohere.com>
2025-02-18 01:17:04 +0100

    chore: Tab to navigate

--- init.lua
+++ init.lua
@@ -205,6 +205,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<Tab>', '<C-w><C-w>', { desc = 'Move to the next window' })
 
 -- Custom keybinds
 vim.keymap.set('i', '<C-a>', '<C-o>^', { desc = 'Move to beginning of line' })
